28a29945e9f7a6916839419509e2f26d8559fde7
Bruno Garcia <41070501+brunograna@users.noreply.github.com>
2019-09-19 11:12:59 -0300

    CRUD Cliente UI integrated

--- src/main/java/br/senac/rj/crm/controller/ClienteController.java
+++ src/main/java/br/senac/rj/crm/controller/ClienteController.java
@@ -1,11 +1,12 @@
 package br.senac.rj.crm.controller;
 
 import br.senac.rj.crm.domain.Cliente;
+import br.senac.rj.crm.service.ClienteService;
+import javassist.tools.rmi.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -14,16 +15,71 @@ import javax.validation.Valid;
 @RequestMapping("/clientes")
 public class ClienteController {
 
+    @Autowired
+    private ClienteService clienteService;
+
     @GetMapping
-    public ModelAndView list(){
+    public ModelAndView listAll(){
         ModelAndView mv = new ModelAndView("/auth/clientes/listarClientes");
+        mv.addObject("clientes", clienteService.findAll());
         return mv;
     }
 
     @PostMapping
-    public ModelAndView save(@RequestBody Cliente cliente){
-        ModelAndView mv = new ModelAndView("redirect:/clientes");
+    public ModelAndView save(@Valid Cliente cliente, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            System.out.println("Has errors");
+            return new ModelAndView("/auth/clientes/cadastrarClientes");
+        }
 
+        ModelAndView mv = new ModelAndView("redirect:/clientes");
+        try{
+            clienteService.save(cliente);
+        }catch (Exception e){
+            mv = new ModelAndView("redirect:/clientes/adicionar?error");
+        }
+        return mv;
+    }
+
+    @GetMapping("/adicionar")
+    public ModelAndView add(){
+        ModelAndView mv = new ModelAndView("/auth/clientes/cadastrarClientes");
+        mv.addObject("cliente", new Cliente());
+        return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView edit(@PathVariable("id") Integer id){
+        ModelAndView mv = new ModelAndView("/auth/clientes/alterarClientes");
+        try {
+            Cliente clienteRetrieved = clienteService.findById(id);
+            mv.addObject("cliente", clienteRetrieved);
+        } catch (ObjectNotFoundException e) {
+            mv = new ModelAndView("redirect:/clientes?error");
+        }
+        return mv;
+    }
+
+
+    @PostMapping("/edit")
+    public ModelAndView saveEdit(@Valid Cliente cliente, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ModelAndView("/auth/clientes/alterarClientes");
+        }
+
+        ModelAndView mv = new ModelAndView("redirect:/clientes/");
+        try {
+            clienteService.update(cliente);
+        } catch (ObjectNotFoundException e) {
+            mv = new ModelAndView("redirect:/clientes?error");
+        }
+        return mv;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public ModelAndView delete(@PathVariable("id") Integer id){
+        ModelAndView mv = new ModelAndView("redirect:/clientes");
+        clienteService.softDelete(id);
         return mv;
     }
 
