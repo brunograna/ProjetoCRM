4e286f1c494823d9b476720abc61e4766a935425
Bruno Garcia <41070501+brunograna@users.noreply.github.com>
2019-09-19 21:02:48 -0300

    Servlet Initializer

---
+++ src/main/java/br/senac/rj/crm/ServletInitializer.java
@@ -0,0 +1,16 @@
+package br.senac.rj.crm;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CrmApplication.class);
+    }
+
+}
+
